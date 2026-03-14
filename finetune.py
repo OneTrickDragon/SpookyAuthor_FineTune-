24eb8d4bb8fab609fad14ab4842a49b170d3c4d6
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-03-14 22:29:13 +0530

    dataset

--- finetune.py
+++ finetune.py
@@ -7,3 +7,27 @@ from transformers import (
 from peft import LoraConfig, get_peft_model
 from trl import SFTTrainer, SFTConfig
 from datasets import load_dataset
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype="float16",
+    bnb_4bit_use_double_quant=True,
+)
+
+model_id = "mistralai/Mistral-7B-v0.1"
+model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device = "auto")
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+peft_config = LoraConfig(
+    r = 16,
+    lora_alpha = 32,
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
+    lora_dropout=0.05,
+    bias = "none",
+    task_type = "CAUSAL_LM"
+)
+
