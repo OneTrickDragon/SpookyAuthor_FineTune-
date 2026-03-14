dff456378abcf1d9a4f5b12cc8ae92e003cef2b5
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-03-14 22:19:56 +0530

    first commit

---
+++ finetune.py
@@ -0,0 +1,9 @@
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    TrainingArguments,
+)
+from peft import LoraConfig, get_peft_model
+from trl import SFTTrainer, SFTConfig
+from datasets import load_dataset
