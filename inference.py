a6167953d98ebc2c60705f94f739a58411663e90
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-03-19 07:26:19 +0530

    training

--- inference.py
+++ inference.py
@@ -1,16 +1,46 @@
 import torch
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer
-)
+import pandas as pd
+import torch.nn.functional as F
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from peft import PeftModel
+from tqdm import tqdm
 
-base_model_id = "mistralai/Mistal-7B-v0.3"
-base_model = AutoModelForCausalLM.from_pretrained(base_model_id, torch_dtype = torch.float16, 
-                                                  device_map="auto")
+base_model_id = "mistralai/Mistral-7B-v0.1"
+adapter_path = "./author_id_adapter"
 
-adapter_path = "/results"
-model = PeftModel.from_pretrained(base_model, adapter_path)
+model = AutoModelForSequenceClassification.from_pretrained(
+    base_model_id, 
+    num_labels=3, 
+    torch_dtype=torch.float16, 
+    device_map="auto"
+)
+model = PeftModel.from_pretrained(model, adapter_path)
+model.eval()
 
 tokenizer = AutoTokenizer.from_pretrained(base_model_id)
+tokenizer.pad_token = tokenizer.eos_token
 
+test_df = pd.read_csv("test.csv")
+results = []
+
+print("Starting inference...")
+with torch.no_grad():
+    for _, row in tqdm(test_df.iterrows(), total=len(test_df)):
+        inputs = tokenizer(row['text'], return_tensors="pt", truncation=True, padding=True).to("cuda")
+
+        outputs = model(**inputs)
+        logits = outputs.logits
+
+        probs = F.softmax(logits, dim=-1).cpu().numpy()[0]
+        
+        results.append({
+            "id": row['id'],
+            "EAP": round(float(probs[0]), 6),
+            "HPL": round(float(probs[1]), 6),
+            "MWS": round(float(probs[2]), 6)
+        })
+
+
+submission_df = pd.DataFrame(results)
+submission_df.to_csv("submission.csv", index=False)
+print("Inference complete! submission.csv created.")
